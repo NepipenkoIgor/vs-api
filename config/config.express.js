62bd8a72477ad0cebd5c6b32f74158359d4bf651
Nepipenkoigor <igornepipenko@gmail.com>
2016-01-20 13:13:18 +0200

    Fix:  public

--- config/config.express.js
+++ config/config.express.js
@@ -3,10 +3,10 @@ var express = require('express');
 module.exports = function (app) {
   app.use(bodyParser.urlencoded({extended: false}));
   app.use(bodyParser.json());
+  app.use(express.static('public'));
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-  app.use(express.static('public'));
 };
