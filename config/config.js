af24880833deba7987e783d2a9d44487e260c318
oleg.rybnikov <oleg.rybnikov@valor-software.com>
2016-10-27 17:38:00 +0300

    add prod url to allowed

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 module.exports = function(app) {
 	const _config = {
 		adminEmail: 'oleg.rybnikov@valor-software.com',
-		allowedOrigins: ['http://localhost:4200']
+		allowedOrigins: ['http://localhost:4200', 'https://vs-work.github.io']
 	};
   app.set('config', _config);
 };
