3a938a3b8aff1e51bc34d69ed918bdd7f0fea040
Oleksii Aleksandrov <alexof3@gmail.com>
2018-11-19 14:51:28 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@
 module.exports = function (app) {
   const _config = {
     adminEmail: 'oleksii.aleksandrov@valor-software.com',
-    allowedOrigins: ['http://localhost:4200', 'http://localhost:4343', 'https://vs-work.github.io']
+    allowedOrigins: ['http://localhost:4200','https://vs-work.github.io']
   };
   app.set('config', _config);
 };
