cd65655f033677b544345196809a8c5bd90b6269
Oleksii Aleksandrov <alexof3@gmail.com>
2018-11-19 09:43:59 +0200

    fix validation issue

--- config/config.express.js
+++ config/config.express.js
@@ -1,16 +1,20 @@
-var bodyParser = require('body-parser');
-var express = require('express');
+const bodyParser = require('body-parser');
+const express = require('express');
+
 module.exports = function (app) {
   app.use(bodyParser.urlencoded({extended: false}));
   app.use(bodyParser.json());
   app.use(express.static('public'));
-  app.use(function(req, res, next) {
+  app.use(function (req, res, next) {
 
-  	const origin = req.headers.origin;
-  	const acceptedOrigins = process.env.ALLOWED_ORIGINS.split(',') || app.get('config').allowedOrigins;
-  	if (!origin || !acceptedOrigins.includes(origin)) {
-  		return res.status(403).send('Forbidden');
-  	}
+    const origin = req.headers.origin;
+    const acceptedOrigins = process.env.ALLOWED_ORIGINS ?
+      process.env.ALLOWED_ORIGINS.split(',') :
+      app.get('config').allowedOrigins;
+
+    if (!origin || !acceptedOrigins.includes(origin)) {
+      return res.status(403).send('Forbidden');
+    }
 
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
